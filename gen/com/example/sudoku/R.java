0c63356a49096acf7e9a2ee80d8271ef0f36afbb
AlexP <alexandrupetrescu94@yahoo.com>
2014-02-24 21:38:32 +0200

    Added bkg and new styles

--- gen/com/example/sudoku/R.java
+++ gen/com/example/sudoku/R.java
@@ -10,6 +10,9 @@ package com.example.sudoku;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f070000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,17 +27,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f080001;
-        public static final int button3=0x7f080002;
-        public static final int button4=0x7f080003;
+        public static final int action_settings=0x7f090005;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int button3=0x7f090003;
+        public static final int button4=0x7f090004;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_sudoku=0x7f030000;
     }
     public static final class menu {
-        public static final int sudoku=0x7f070000;
+        public static final int sudoku=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -42,6 +46,7 @@ public final class R {
         public static final int continue_label=0x7f050003;
         public static final int hello_world=0x7f050002;
         public static final int highscore_label=0x7f050006;
+        public static final int name_of_the_game=0x7f050007;
         public static final int new_game_label=0x7f050004;
         public static final int quit_label=0x7f050005;
     }
